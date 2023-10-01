ef4cf71053c4e478be791bf700cc74ad2aa9447b
HEXACTA\ffunes <ffunes@hexacta.com>
2023-10-01 12:01:05 -0300

    fix: add column type to Promedio column

--- Data/PAIIDbContext.cs
+++ Data/PAIIDbContext.cs
@@ -9,6 +9,15 @@ public class PAIIDbContext : DbContext
     {
  
     }
- 
+
     public DbSet<Alumnos> Alumnos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Alumnos>()
+            .Property(a => a.Promedio)
+            .HasColumnType("decimal(5, 2)"); // Specify the SQL Server column type here
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
